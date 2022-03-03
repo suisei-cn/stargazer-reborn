c5ec5359b998b3f49925ee2b655e665ebe898d1d
LightQuantum <self@lightquantum.me>
2022-03-03 18:40:20 +0800

    feat(twitter): don't fetch image

--- workers/twitter/src/models.rs
+++ workers/twitter/src/models.rs
@@ -1,45 +1,25 @@
 use egg_mode::entities::MediaType;
-use eyre::{Report, Result};
-use futures_util::future::join_all;
-use once_cell::sync::Lazy;
-use reqwest::Client;
 use serde::{Deserialize, Serialize};
-use tap::TapFallible;
-use tracing::error;
-
-static CLIENT: Lazy<Client> = Lazy::new(Client::new);
 
 #[derive(Clone, Debug, Eq, PartialEq, Serialize, Deserialize, Hash)]
 pub struct Tweet {
     pub id: u64,
     pub text: String,
-    pub photos: Vec<Vec<u8>>,
+    pub photos: Vec<String>,
     pub link: String,
     pub is_rt: bool,
 }
 
-impl Tweet {
-    pub async fn from_raw(tweet: egg_mode::tweet::Tweet) -> Self {
-        let photos = join_all(
-            tweet
-                .entities
-                .media
-                .into_iter()
-                .flatten()
-                .filter(|medium| medium.media_type == MediaType::Photo)
-                .map(|medium| medium.media_url_https)
-                .map(|url| async move {
-                    let resp = CLIENT.get(url).send().await?;
-                    Result::<_, Report>::Ok(resp.bytes().await?.to_vec())
-                }),
-        )
-        .await
-        .into_iter()
-        .filter_map(|img| {
-            img.tap_err(|e| error!(error=?e, "Failed to download tweet image"))
-                .ok()
-        })
-        .collect();
+impl From<egg_mode::tweet::Tweet> for Tweet {
+    fn from(tweet: egg_mode::tweet::Tweet) -> Self {
+        let photos = tweet
+            .entities
+            .media
+            .into_iter()
+            .flatten()
+            .filter(|medium| medium.media_type == MediaType::Photo)
+            .map(|medium| medium.media_url_https)
+            .collect();
 
         Self {
             id: tweet.id,
