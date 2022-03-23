8ffd453630a84ab8194ec2ff254a7d5031f99946
George Miao <gm@miao.dev>
2022-03-23 19:50:28 -0400

    feat(api): Context helper methods and comment

--- api/src/server/context.rs
+++ api/src/server/context.rs
@@ -1,9 +1,13 @@
+//! Context of the server. Contains the configuration and database handle.
 use std::sync::Arc;
 
-use mongodb::Collection;
+use mongodb::{bson::doc, Collection};
 use sg_core::models::{Entity, Group, Task, User};
 
-use crate::server::{JWTContext, DB};
+use crate::{
+    rpc::ApiError,
+    server::{config::Config, JWTContext, DB},
+};
 
 #[derive(Debug, Clone)]
 /// Context being shared between handlers. This will be cloned every time a handler is called.
@@ -11,9 +15,13 @@ use crate::server::{JWTContext, DB};
 pub struct Context {
     /// DB instance. Since DB is composed of [`Collection`](mongodb::Collection)s, cloning is cheap.
     pub db: DB,
+    /// JWT context, used to decode, encode and validate JWT tokens.
     pub jwt: Arc<JWTContext>,
+    /// Config.
+    pub config: Arc<Config>,
 }
 
+/// Context of the server. Contains the configuration and database handle.
 impl Context {
     pub fn users(&self) -> &Collection<User> {
         &self.db.users
@@ -29,5 +37,20 @@ impl Context {
 
     pub fn groups(&self) -> &Collection<Group> {
         &self.db.groups
+    }
+
+    pub async fn find_user(&self, id: &uuid::Uuid) -> Result<User, ApiError> {
+        self.users()
+            .find_one(doc! { "id": &id }, None)
+            .await?
+            .ok_or_else(|| ApiError::user_not_found(id.to_string()))
+    }
+
+    pub fn auth_password(&self, password: impl AsRef<str>) -> Result<(), ApiError> {
+        if password.as_ref() != self.config.bot_password {
+            Err(ApiError::wrong_password())
+        } else {
+            Ok(())
+        }
     }
 }
