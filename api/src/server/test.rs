a7d7e9dea898597eb3a9695a524fa607c29ab20d
George Miao <gm@miao.dev>
2022-03-24 23:29:51 -0400

    test(api): shim

--- api/src/server/test.rs
+++ api/src/server/test.rs
@@ -1,15 +1,15 @@
 use crate::{
     rpc::{
-        models::{AddUser, DelUser, Entities, GetEntities, GetUser, Null},
-        ApiError, Request, Response, ResponseObject,
+        models::{AddUser, DelUser, GetEntities, GetUser, NewSession, UpdateUserSetting},
+        ApiError, ApiResult, Request, ResponseObject,
     },
     server::{serve_with_config, Config},
 };
 
-use std::{sync::Once, time::Duration};
+use std::{collections::HashSet, sync::Once, thread::available_parallelism, time::Duration};
 
 use color_eyre::{eyre::Context, Result};
-use serde::{de::DeserializeOwned, Serialize};
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use sg_core::models::{EventFilter, User};
 use tracing::metadata::LevelFilter;
 
@@ -25,28 +25,43 @@ fn prep() {
 
         color_eyre::install().unwrap();
 
-        // Spawn a server into background, which ideally will be destroyed when all tests are done.
+        // Spawn a server into background, which ideally will be destroyed when all tests are finished.
         std::thread::spawn(|| {
             tokio::runtime::Builder::new_multi_thread()
-                .worker_threads(4)
+                .worker_threads(available_parallelism().unwrap().into())
                 .enable_all()
                 .build()
                 .unwrap()
                 .block_on(serve_with_config(Config {
                     bind: "127.0.0.1:8080".parse().unwrap(),
-                    session_timeout: Duration::from_secs(0),
+                    token_timeout: Duration::from_secs(0),
                     mongo_uri: "mongodb://192.168.1.53:27017".to_owned(),
                     ..Default::default()
                 }))
         });
     });
-
-    std::thread::sleep(Duration::from_secs(1));
 }
 
-fn call<B: Request + Serialize, R: Response + DeserializeOwned>(
-    body: B,
-) -> Result<ResponseObject<R>> {
+fn call<R: Request + Serialize>(body: R) -> Result<ApiResult<R::Res>>
+where
+    R::Res: DeserializeOwned,
+{
+    #[derive(Serialize, Deserialize)]
+    #[serde(untagged)]
+    enum Shim<R> {
+        Ok(R),
+        Err(ApiError),
+    }
+
+    impl<T> From<Shim<T>> for ApiResult<T> {
+        fn from(shim: Shim<T>) -> Self {
+            match shim {
+                Shim::Ok(res) => ApiResult::Ok(res),
+                Shim::Err(err) => ApiResult::Err(err),
+            }
+        }
+    }
+
     let res = reqwest::blocking::Client::builder()
         .build()?
         .post("http://127.0.0.1:8080/v1")
@@ -58,7 +73,11 @@ fn call<B: Request + Serialize, R: Response + DeserializeOwned>(
         .wrap_err("Failed to read response")?;
 
     tracing::info!(res = res.as_str());
-    serde_json::from_str(&res).wrap_err("Failed to deserialize")
+    let serialized = serde_json::from_str::<ResponseObject<Shim<R::Res>>>(&res)
+        .wrap_err("Failed to deserialize")
+        .unwrap();
+
+    Ok(serialized.data.into())
 }
 
 #[test]
@@ -72,7 +91,7 @@ fn test_new_user() {
         name: "Pop".to_owned(),
     };
 
-    let res1 = call::<_, User>(req).unwrap();
+    let res1 = call(req).unwrap().unwrap();
 
     let User {
         id,
@@ -80,7 +99,7 @@ fn test_new_user() {
         name,
         avatar,
         event_filter,
-    } = &res1.data;
+    } = &res1;
 
     assert_eq!(im, "tg");
     assert_eq!(name, "Pop");
@@ -100,9 +119,9 @@ fn test_new_user() {
         user_id: id.to_owned().into(),
     };
 
-    let res2 = call::<_, User>(req).unwrap();
+    let res2 = call(req).unwrap().unwrap();
 
-    assert_eq!(res1.data, res2.data);
+    assert_eq!(res1, res2);
 
     // Delete the new user
     let req = DelUser {
@@ -110,8 +129,15 @@ fn test_new_user() {
         password: "TEST".to_owned(),
     };
 
-    let res = call::<_, Null>(req).unwrap().is_success();
-    assert!(res)
+    let res = call(req).unwrap().is_ok();
+    assert!(res);
+
+    // Verify that the user is no longer in the database
+    assert!(call(GetUser {
+        user_id: id.to_owned().into(),
+    })
+    .unwrap()
+    .is_err());
 }
 
 #[test]
@@ -125,10 +151,13 @@ fn test_new_user_wrong_password() {
         name: "Pop".to_owned(),
     };
 
-    let obj = call::<_, ApiError>(req).unwrap();
+    let obj = call(req).unwrap();
 
-    assert!(!obj.success);
-    assert!(obj.data.error.contains(&"Wrong password".to_owned()));
+    assert!(obj.is_err());
+    assert!(obj
+        .unwrap_err()
+        .error
+        .contains(&"Wrong password".to_owned()));
 }
 
 #[test]
@@ -137,10 +166,10 @@ fn test_get_entities() {
 
     let req = GetEntities {};
 
-    let res = call::<_, Entities>(req).unwrap();
+    let res = call(req).unwrap();
 
-    assert!(res.success);
-    tracing::info!(entities = ?res.data);
+    assert!(res.is_ok());
+    tracing::info!(entities = ?res);
 }
 
 #[test]
@@ -154,10 +183,54 @@ fn test_delete_nonexist_user() {
         password: "TEST".to_owned(),
     };
 
-    let res = call::<_, ApiError>(req).unwrap();
-    assert!(res.is_error());
+    let res = call(req).unwrap();
+    assert!(res.is_err());
     assert!(res
-        .data
+        .unwrap_err()
         .error
         .contains(&format!("Cannot find user with ID `{id}`")));
+}
+
+#[test]
+fn test_update_user_settings() {
+    prep();
+
+    let user = AddUser {
+        im: "tg".to_owned(),
+        avatar: "http://placekitten.com/114/514".parse().unwrap(),
+        password: "TEST".to_owned(),
+        name: "Pop".to_owned(),
+    };
+
+    let User { id, .. } = call(user).unwrap().unwrap();
+
+    let token = call(NewSession {
+        password: "TEST".to_owned(),
+        user_id: id.into(),
+    })
+    .unwrap()
+    .unwrap()
+    .token;
+
+    let event_filter = EventFilter {
+        entities: HashSet::from_iter(["a1e28c88-be24-48b0-b18a-81531e669905"
+            .parse::<uuid::Uuid>()
+            .unwrap()
+            .into()]),
+        kinds: HashSet::from_iter(["twitter/new_tweet".to_owned()]),
+    };
+
+    let update = UpdateUserSetting {
+        user_id: id.into(),
+        token,
+        event_filter: event_filter.clone(),
+    };
+
+    let res = call(update).unwrap();
+
+    assert!(res.is_ok());
+
+    let user = call(GetUser { user_id: id.into() }).unwrap().unwrap();
+
+    assert_eq!(user.event_filter, event_filter);
 }
