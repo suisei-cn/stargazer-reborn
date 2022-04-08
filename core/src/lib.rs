d8dd399f5cb52d52d19b642b90be193184a28235
LightQuantum <self@lightquantum.me>
2022-04-08 10:53:41 +0800

    chore: suppress chore warning

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,5 +1,5 @@
 //! Core definitions and protocols for Stargazer.
-#![allow(clippy::module_name_repetitions)]
+#![allow(clippy::module_name_repetitions, clippy::default_trait_access)]
 #![deny(missing_docs)]
 
 pub mod adapter;
