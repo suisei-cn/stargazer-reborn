2023867570f0e590588f2f636f2416b671708ae3
LightQuantum <self@lightquantum.me>
2022-04-08 21:25:36 +0800

    feat(middleware): cancel a delayed delivery

--- middlewares/delay/src/main.rs
+++ middlewares/delay/src/main.rs
@@ -8,13 +8,15 @@ use std::sync::Arc;
 use chrono::NaiveDateTime;
 use diesel::r2d2::{ConnectionManager, Pool};
 use diesel::SqliteConnection;
-use eyre::Context;
 use eyre::Result;
+use eyre::{Context, ContextCompat};
 use futures_util::StreamExt;
-use tracing::info;
+use tap::Pipe;
+use tracing::{error, info};
 use tracing_subscriber::EnvFilter;
 
-use sg_core::mq::{MessageQueue, RabbitMQ};
+use sg_core::models::Event;
+use sg_core::mq::{MessageQueue, Middlewares, RabbitMQ};
 
 use crate::config::Config;
 use crate::db::DelayedMessage;
@@ -53,20 +55,49 @@ async fn main() -> Result<()> {
     scheduler.cleanup();
     scheduler.load();
 
-    while let Some(Ok((next, mut event))) = consumer.next().await {
-        info!(event_id = %event.id, ?next, "Received event");
+    while let Some(Ok((next, event))) = consumer.next().await {
+        let event_id = event.id;
+        info!(%event_id, ?next, "Received event");
 
-        if let (Some(id), Some(deliver_at)) = (
-            event.fields.remove("x-delay-id").and_then(|v| v.as_i64()),
-            event
-                .fields
-                .remove("x-delay-at")
-                .and_then(|v| v.as_i64())
-                .map(|i| NaiveDateTime::from_timestamp(i, 0)),
-        ) {
-            let delayed_message = DelayedMessage::new(id, next, event, deliver_at);
-            scheduler.add_task(delayed_message, true);
+        if let Err(error) = handle_event(next, event, &scheduler) {
+            error!(%event_id, ?error, "Failed to process event");
         }
     }
+    Ok(())
+}
+
+fn handle_event(next: Middlewares, mut event: Event, scheduler: &Arc<Scheduler>) -> Result<()> {
+    let id = event
+        .fields
+        .remove("x-delay-id")
+        .wrap_err("Missing `x-delay-at`")?
+        .as_i64()
+        .wrap_err("Not a integer: `x-delay-at`")?;
+
+    let cancel = if let Some(cancel) = event.fields.remove("x-delay-cancel") {
+        // If `x-delay-cancel` is set to true, we cancel the task
+        cancel
+            .as_bool()
+            .wrap_err("Not a boolean: `x-delay-cancel`")?
+    } else {
+        // There's no `x-delay-cancel` field, so this is a new delayed message
+        false
+    };
+
+    if cancel {
+        scheduler.remove_task(id);
+    } else {
+        let deliver_at = event
+            .fields
+            .remove("x-delay-at")
+            .wrap_err("Missing `x-delay-at`")?
+            .as_i64()
+            .wrap_err("Not a timestamp: `x-delay-at`")?
+            .pipe(|ts| NaiveDateTime::from_timestamp(ts, 0));
+
+        let msg = DelayedMessage::new(id, next, event, deliver_at);
+        scheduler.add_task(msg, true);
+    }
+
     Ok(())
 }
