b6c632a60ffdedc2376d93f4394900da84a4c0ae
LightQuantum <self@lightquantum.me>
2022-06-23 08:23:51 +0800

    feat(api/server): tracing env filter

--- api/src/bin/server.rs
+++ api/src/bin/server.rs
@@ -1,9 +1,11 @@
 use tracing::metadata::LevelFilter;
+use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
 async fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
     tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
         .with_max_level(LevelFilter::DEBUG)
         .init();
 
