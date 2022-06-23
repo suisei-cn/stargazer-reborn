1a1d1af3d53338292e4178aec12ff47d2de3ea58
LightQuantum <self@lightquantum.me>
2022-06-23 08:06:10 +0800

    chore(api): remove unused util function

--- api/src/utils.rs
+++ api/src/utils.rs
@@ -1,13 +1,6 @@
-use serde_json::Value;
 use std::time::SystemTime;
 
 #[must_use]
 pub fn timestamp() -> String {
     humantime_serde::re::humantime::format_rfc3339(SystemTime::now()).to_string()
-}
-
-pub fn map(k: impl Into<String>, v: impl Into<String>) -> serde_json::Map<String, Value> {
-    let mut map = serde_json::Map::new();
-    map.insert(k.into(), Value::String(v.into()));
-    map
 }
